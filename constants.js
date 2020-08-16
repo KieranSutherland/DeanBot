86b9884f2233e2fce9bfa2014b050db7dd9e658f
Harry morgan <harrywm12@gmail.com>
2020-08-16 15:54:56 +0100

    Tells dean to get back to work when he joins a voice channel

--- constants.js
+++ constants.js
@@ -1,2 +1,15 @@
+// Client login key
+export const loginKey = 'NzQ0MTc1ODUwODMxODcyMDgw.XzfZxA.x5EstcgbGmduhWrHinzC9EILSSA';
+
+// Command prefix
 export const prefix = '!';
+
+// Channel IDs
+export const normies = '257622197420556288';
+
+// Role Ids
 export const deanRoleId = '732595910344704141';
+export const leMemeSupremeRoleId = '257637882112507904';
+
+// User Id
+export const deanUserId = '705420522313220097';
