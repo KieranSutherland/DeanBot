3ceb6b25d4bea80ab6b211f2fa65db10bac421e4
Kieran Sutherland <35941396+KieranSutherland@users.noreply.github.com>
2020-08-19 21:20:03 +0100

    Add missing import

--- voiceStateCommands/backToWorkDeanVoiceJoin.js
+++ voiceStateCommands/backToWorkDeanVoiceJoin.js
@@ -1,4 +1,5 @@
 import * as constants from './../constants.js';
+import * as commandsUtil from './../commandsUtil.js';
 
 export const backToWorkDeanVoiceJoin = (oldMember, newMember) => {
     if(!oldMember.channel && newMember.channel && oldMember.id == constants.deanUserId) {
