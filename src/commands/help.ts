a664d79a373137c168a866b22bded1cea800d2be
Kieran Sutherland <35941396+KieranSutherland@users.noreply.github.com>
2020-08-26 20:36:44 +0100

    Change formatting of help display

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,11 +1,11 @@
 const helpMessage: string = '```'
                     + 'Need help do you, little bitch? Here are the current supported commands:'
-                    + '\n\t!insult - send a random insult about Dean'
-                    + '\n\t!pic - send a random pic of Dean'
-                    + '\n\t!msgToChannel [channel name] [message] - sends your custom message to the specified channel'
-                    + '\n\t!fart - play a random fart noise in the voice channel the member is currently in'
-                    + '\n\t!exile [name] - exiles the specified user (use our irl names)'
-                    + '\n\t!activity [activity] - sets an activity for the bot to be shown doing (specify no activity to reset it)'
+                    + '\n\n!insult - send a random insult about Dean'
+                    + '\n\n!pic - send a random pic of Dean'
+                    + '\n\n!msgToChannel [channel name] [message] - sends your custom message to the specified channel'
+                    + '\n\n!fart - play a random fart noise in the voice channel the member is currently in'
+                    + '\n\n!exile [name] - exiles the specified user (use our irl names)'
+                    + '\n\n!activity [activity] - sets an activity for the bot to be shown doing (specify no activity to reset it)'
                     + '```';
 
 export const help = (message: any) => message.channel.send(helpMessage);
