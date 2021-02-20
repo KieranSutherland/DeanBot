15d6a10cf8c9e27dca5e95e4ec97b70b7490576f
Kieran Sutherland <35941396+KieranSutherland@users.noreply.github.com>
2021-02-20 14:27:04 +0000

    Fix say command with GoogleTTS API change

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -1,25 +1,28 @@
-import GoogleTTS from 'google-tts-api';
+import * as googleTTS from 'google-tts-api';
 import { Message, VoiceChannel } from 'discord.js';
 
 export const say = async (message: Message) => {
     let args = message.content.split(' ');
-    if(args.length < 2) {
+    if (args.length < 2) {
         message.channel.send('```Error: gotta specify a sentence to say, retard\nUse !help for help```');
         return;
     }
-    
+
     let sentence: string = message.content.substr(args[0].length + 1, message.content.length);
 
     let voiceChannel: VoiceChannel | null | undefined = message.member?.voice.channel;
-    if(!voiceChannel) return;
+    if (!voiceChannel) return;
 
     const connection = await voiceChannel.join();
     // Go to for supported language codes: https://cloud.google.com/speech-to-text/docs/languages
-    GoogleTTS(sentence, 'en-GB', 1) // speed normal = 1
-        .then(function (url: any) {
-            connection.play(url);
-        })
-        .catch(function (err: any) {
-            console.error(err.stack);
+    try {
+        const url = googleTTS.getAudioUrl(sentence, {
+            lang: 'en-GB',
+            slow: false,
+            host: 'https://translate.google.com',
         });
+        connection.play(url);
+    } catch (err: any) {
+        console.error(err.stack);
+    }
 }
